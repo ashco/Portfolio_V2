2638b97980ddd17c1940f79b9235ad9cbb9b6c58
Ashton <ashchristie@gmail.com>
2018-04-05 15:32:42 -0700

    adding in javascript nav scrolling and show/hide nav item

---
+++ source/source.js
@@ -0,0 +1,31 @@
+const sectionWelcome = document.querySelector('#welcome');
+const sectionProjects = document.querySelector('#projects');
+const sectionAbout = document.querySelector('#about');
+
+const titleName = document.querySelector('.welcome__title');
+const navName = document.querySelector('li.nav__menu-item--name');
+
+function scrollPage(section) {
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+}
+
+function toggleNavName() {
+  const windowInfo = window.scrollY;
+  const titleOffsetBottom = titleName.offsetTop + titleName.clientHeight;
+  if (windowInfo > titleOffsetBottom) {
+    navName.classList.remove('hidden');
+  } 
+  else {
+    navName.classList.add('hidden');
+  }
+}
+
+window.addEventListener('scroll', toggleNavName);
+
+
+// const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+// // bottom of the image
+// const imageBottom = sliderImage.offsetTop + sliderImage.height;
